c9fefce4a645f3bc13536c542a5d9c26e9199f5e
Álvaro Patrício <alvaropatricio61@gmail.com>
2021-02-08 17:09:20 -0300

    Projeto aula 3 finalizado

--- scripts.js
+++ scripts.js
@@ -8,24 +8,18 @@ const Modal = {
   },
 };
 
+const Storage = {
+  get() {
+    return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+  },
+  set(transactions) {
+    localStorage.setItem("dev.finances:transactions", 
+    JSON.stringify(transactions));
+  },
+}
+
 const Transaction = {
-  all: [
-    {
-      description: "Luz",
-      amount: -50000,
-      date: "23/01/2021",
-    },
-    {
-      description: "Website",
-      amount: 500000,
-      date: "23/01/2021",
-    },
-    {
-      description: "Internet",
-      amount: -20000,
-      date: "23/01/2021",
-    },
-  ],
+  all: Storage.get(),
   add(transaction) {
     Transaction.all.push(transaction);
     App.reload();
@@ -61,11 +55,12 @@ const DOM = {
   transactionsContainer: document.querySelector("#data-table tbody"),
   addTransaction(transaction, index) {
     const tr = document.createElement("tr");
-    tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+    tr.innerHTML = DOM.innerHTMLTransaction(transaction, index);
+    tr.dataset.index = index;
 
     DOM.transactionsContainer.appendChild(tr);
   },
-  innerHTMLTransaction(transaction) {
+  innerHTMLTransaction(transaction, index) {
     const CSSclass = transaction.amount > 0 ? "income" : "expense";
 
     const amount = Utils.formatCurrency(transaction.amount);
@@ -75,7 +70,9 @@ const DOM = {
    <td class="description">${transaction.description}</td>
    <td class=${CSSclass}>${amount}</td>
    <td class="date">${transaction.date}</td>
-   <td><img src="./assets/minus.svg" alt="Remover transação" /></td>
+   <td>
+    <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação" />
+   </td>
   </tr>
   `;
     return html;
@@ -185,10 +182,10 @@ const Form = {
 
 const App = {
   init() {
-    Transaction.all.forEach((transaction) => {
-      DOM.addTransaction(transaction);
-    });
+    Transaction.all.forEach(DOM.addTransaction);
     DOM.updateBalance();
+
+    Storage.set(Transaction.all)
   },
   reload() {
     DOM.clearTransactions();
